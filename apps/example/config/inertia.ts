437ebfc0be6c9e8ce841297ba8637405b9eb2edf
Walaff Fernandes <walaffonofre@icloud.com>
2024-05-10 20:28:04 -0300

    chore: update build command and add typecheck script

---
+++ apps/example/config/inertia.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from '@adonisjs/inertia'
+
+export default defineConfig({
+  /**
+   * Path to the Edge view that will be used as the root view for Inertia responses
+   */
+  rootView: 'inertia_layout',
+
+  /**
+   * Data that should be shared with all rendered pages
+   */
+  sharedData: {
+    errors: (ctx) => ctx.session?.flashMessages.get('errors'),
+  },
+
+  /**
+   * Options for the server-side rendering
+   */
+  ssr: {
+    enabled: true,
+    entrypoint: 'inertia/app/ssr.tsx'
+  }
+})
