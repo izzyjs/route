ef5a399025966565205aa9664a2a3c14a2111f67
Walaff Fernandes <walaffonofre@icloud.com>
2024-05-11 01:03:08 -0300

    feat: refactor route function to improve readability and maintainability

---
+++ src/client.ts
@@ -0,0 +1,47 @@
+import { ExcludeName, ExtractName, Params } from './types/routes.js'
+import { Route, Routes } from './route.js'
+
+/**
+ * Resolve a route path by its name and parameters
+ * @param routeName The name of the route
+ * @param params The parameters of the route
+ * @returns The resolved route path
+ * @example
+ * ```ts
+ * route('users.show', { id: '1' })
+ * // => "/users/1"
+ * ```
+ */
+export function route<Name extends ExtractName>(
+  routeName: Name,
+  options: {
+    params: Params<Name>
+    qs?: Record<string, any>
+    prefix?: string
+  }
+): Route
+
+/**
+ * Resolve a route path by its name
+ * @param routeName The name of the route
+ * @returns The resolved route path
+ * @example
+ * ```ts
+ * route('users.index')
+ * // => "/users"
+ * ```
+ */
+export function route<Name extends ExcludeName['name']>(
+  routeName: Name,
+  options?: {
+    params?: never
+    qs?: Record<string, any>
+    prefix?: string
+  }
+): Route
+
+export function route(): Routes
+
+export function route(routeName?: unknown, options?: any): Route | Routes {
+  return Route.new(routeName, options?.params, options?.qs, options?.prefix)
+}
