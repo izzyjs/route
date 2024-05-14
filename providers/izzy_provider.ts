f9ef021a172e3d595c451579a1e0c0aef9384c02
Lahgolz <lahgolz@proton.me>
2024-05-14 21:12:21 +0200

    fix: retrieve routes in test environment

--- providers/izzy_provider.ts
+++ providers/izzy_provider.ts
@@ -5,9 +5,10 @@
  * For the full license information, please view the LICENSE file that was distributed with this source code.
  */
 
-import type { ApplicationService } from '@adonisjs/core/types'
+import type { ApplicationService, HttpRouterService } from '@adonisjs/core/types'
 import type { SerializedRoute } from '../src/types/manifest.js'
 import { serializeRoute } from '../src/serialize_route.js'
+import { RouteJSON } from '@adonisjs/core/types/http'
 
 declare global {
   namespace globalThis {
@@ -39,12 +40,38 @@ export default class IzzyRouteProvider {
       })
     }
 
+    if (routesJSON.length === 0) {
+      routesJSON = await this.#getTestRoutes(router)
+    }
+
     const exists = routesJSON.find((route) => route.domain === 'root')
 
     if (exists) {
       this.#registerSsrRoutes(exists.routes)
       await this.#registerEdgePlugin(exists.routes)
     }
+  }
+
+  async #getTestRoutes(router: HttpRouterService) {
+    const routes = router.routes
+      .map((route) =>
+        'route' in route ? route.route?.toJSON() : 'toJSON' in route ? route.toJSON() : null
+      )
+      .filter((route): route is RouteJSON => route !== null)
+    const domains = [...new Set(routes.map((route) => route.domain)).values()]
+    const routesJSON: { domain: string; routes: SerializedRoute[] }[] = []
+
+    for (let domain of domains) {
+      const domainRoutes = routes.filter((route) => route.domain === domain)
+      const serializedDomainRoutes = await Promise.all(domainRoutes.map(serializeRoute))
+
+      routesJSON.push({
+        domain,
+        routes: serializedDomainRoutes,
+      })
+    }
+
+    return routesJSON
   }
 
   /**
