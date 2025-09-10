727914bf0487a6d860dc199f12576e85aeb57673
Walaff Fernandes <walaffonofre@icloud.com>
2025-09-10 00:07:23 -0300

    feat: added support for expectation types in tests
    - Included the `@japa/expect-type` package in `package.json` and updated the `test.ts` file to use the `expectTypeOf()` plugin in tests, improving type checking and test robustness.

--- bin/test.ts
+++ bin/test.ts
@@ -1,11 +1,12 @@
 import { assert } from '@japa/assert'
+import { expectTypeOf } from '@japa/expect-type'
 import { configure, processCLIArgs, run } from '@japa/runner'
 
 processCLIArgs(process.argv.splice(2))
 
 configure({
   files: ['tests/**/*.spec.ts'],
-  plugins: [assert()],
+  plugins: [assert(), expectTypeOf()],
 })
 
 run()
